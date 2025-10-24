70a6d63d8f87e5d762b26eb264485dc3d3736ddf
tarun-1507 <tarunbandari1507@gmail.com>
2025-10-25 01:02:40 +0530

    second

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,7 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = ``
+const resumeLink = `https://raw.githubusercontent.com/tarun-1507/tarun-port-folio/main/src/assets/Tarun.sd.pdf`;
 
 
 const Resume = () => {
