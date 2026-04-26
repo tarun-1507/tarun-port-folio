4cee95cedd2a15341534c6853d3b3264938ace49
tarun-1507 <tarunbandari1507@gmail.com>
2026-04-26 19:54:06 +0530

    changes

---
+++ src/components/Skillset/Techstack.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiJava,
+  DiReact,
+  DiGit,
+} from "react-icons/di";
+import {
+  SiTailwindcss,
+  SiSpringboot,
+  SiRedux,
+  SiMongodb,
+  SiHtml5,
+  SiCss3
+} from "react-icons/si";
+
+const Techstack = () => {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+    </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpringboot />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
